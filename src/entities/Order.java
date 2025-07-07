7aadd69a7d216b343d9386e499d09d858ed07612
Caio Santana <caiosantana.lkd@gmail.com>
2025-07-07 15:13:17 -0300

    Created enum OrderStatus and class Order

---
+++ src/entities/Order.java
@@ -0,0 +1,12 @@
+package entities;
+
+import java.time.Instant;
+
+public class Order {
+	private Long id;
+	private Double latitide;
+	private Double longitude;
+	private Instant moment;
+	private OrderStatus status ;
+	 
+}
